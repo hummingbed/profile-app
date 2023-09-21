b33af52728dfb0b9246b32a4f82b5915b5bd667f
MichaelMenebraye <michael@dufuna.org>
2023-09-21 10:09:34 +0100

    update component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import AllUsers from './component/UsersComponent';
+import UsersComponent from './component/UsersComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="flex justify-center bg-red-50">
-      <AllUsers />
+      <UsersComponent />
     </div>
   );
 }
--- src/sliceMachine/rocketSlice.js
+++ src/sliceMachine/rocketSlice.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const API_URL = 'https://random-data-api.com/api/v2/users?size=10&is_xml=true';
 
 export const fetchUserProfile = createAsyncThunk(
-  'rockets/fetchUserProfile',
+  'fetchUserProfile',
   async () => {
     const response = await axios.get(API_URL);
     return response.data;
@@ -17,9 +17,8 @@ const initialState = {
   error: null,
 };
 
-
 const rocketsSlice = createSlice({
-  name: 'rockets',
+  name: 'users',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
@@ -29,7 +28,7 @@ const rocketsSlice = createSlice({
       })
       .addCase(fetchUserProfile.fulfilled, (state, action) => {
         state.status = 'succeeded';
-        state.rockets = action.payload;
+        state.users = action.payload;
       })
       .addCase(fetchUserProfile.rejected, (state, action) => {
         state.status = 'failed';
@@ -38,7 +37,7 @@ const rocketsSlice = createSlice({
   },
 });
 
-export const getAllUserProfileData = (state) => state.rockets.rockets;
+export const getAllUserProfileData = (state) => state.rockets.users;
 export const getAllUserProfileStatus = (state) => state.rockets.status;
 export const getAllUserProfileError = (state) => state.rockets.error;
 
